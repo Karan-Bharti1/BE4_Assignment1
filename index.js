89c9ab29604e1e6d40727d79dd518194928eed9e
Karan-Bharti1 <karan.codes1@gmail.com>
2024-12-19 14:51:33 +0530

    feat:Adding two apis to fetch all the books data by authorName and genre

--- index.js
+++ index.js
@@ -67,6 +67,46 @@ app.get("/books/:bookTitle",async(req,res)=>{
        res.status(500).json({error:"Failed to fetch the data"}) 
     }
 })
+async function readBooksByAuthor(authorName){
+    try {
+      const books=await BooksModel.find({author:authorName})  
+      return books
+    } catch (error) {
+        throw error
+    }
+}
+app.get("/books/author/:authorName",async(req,res)=>{
+    try {
+    const books=await readBooksByAuthor(req.params.authorName) 
+    if(books.length!=0){
+        res.status(201).json(books)
+    }   else{
+        res.status.json(404).json({error:"Books data not found."})
+    }
+    } catch (error) {
+        res.status(500).json({error:"Failed to fetch the data"}) 
+    }
+})
+async function readAllBooksByGenre(genreName) {
+    try {
+        const books=await BooksModel.find({genre:genreName})
+        return books
+    } catch (error) {
+        throw error
+    }
+}
+app.get("/books/genre/:genreName",async(req,res)=>{
+    try {
+        const books=await readAllBooksByGenre(req.params.genreName)
+        if(books.length!=0){
+            res.status(201).json(books)
+        }else{
+            res.status(404).json({error:"Books not found."})
+        }
+    } catch (error) {
+        res.status(500).json({error:"Failed to fetch data "})
+    }
+})
 app.listen(PORT,()=>{
     console.log("App is running on port ",PORT)
 })
